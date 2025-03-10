610b4c9103d75748fc2f7b2fe2ff46d0ac2b6fcb
Felix Leprohon <2268157@bdeb.qc.ca>
2025-03-10 09:44:46 -0400

    servo

--- HardwareController.py
+++ HardwareController.py
@@ -1,4 +1,6 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
+from Status import Status
+import time
 
 class HardwareController:
 
@@ -6,18 +8,28 @@ class HardwareController:
 
     def __init__(self, test_mode=False):
         self.is_test_mode = test_mode
-        # GPIO.setmode(GPIO.BCM)
+        GPIO.setmode(GPIO.BCM)
 
         # self.led = 1
-        # self.alarm = 2
-        # self.servo = 3
-        # self.temp_sensor = 4
+        self.buzzer = 17
+        self.servo = 27
+        self.temp_sensor = 4
+        
+        self.tones = [440, 1440]
+        self.angles = [3, 12]
 
         # GPIO.setup(self.led, GPIO.OUT)
-        # GPIO.setup(self.alarm, GPIO.OUT)
-        # GPIO.setup(self.servo, GPIO.OUT)
-        # GPIO.setup(self.temp_sensor, GPIO.IN)
-
+        GPIO.setup(self.buzzer, GPIO.OUT)
+        GPIO.setup(self.servo, GPIO.OUT)
+        GPIO.setup(self.temp_sensor, GPIO.IN)
+        
+        self.alarm = GPIO.PWM(self.buzzer, 440)
+        self.door = GPIO.PWM(self.servo, 50)
+        
+        self.door.start(0)
+        
+        self.status = Status.NONE
+        
     def read_temp(self):
         return 20 # Read from GPIO
 
@@ -25,15 +37,22 @@ class HardwareController:
         current_temp = self.read_temp()
 
         if not self.is_test_mode:
-            if current_temp > self.MAXIMUM_SAFE_TEMP:
+            if current_temp > self.MAXIMUM_SAFE_TEMP and self.status != Status.ALERT:
+                print('Alert temp')
+                self.status = Status.ALERT
                 ui_callback_function("ALERT")
-                # sound alarm
-                # door action
-                # turn LED ON
-            else:
+                self.alarm.start(50)
+                self.door.start(self.angles[0])
+                time.sleep(0.25)
+                self.door.stop()
+                # turn LED ON              
+            elif current_temp <= self.MAXIMUM_SAFE_TEMP and self.status != Status.SAFE:
                 print('Safe temp')
-                #  alarm
-                # door action
+                self.status = Status.SAFE
+                self.alarm.stop()
+                self.door.start(self.angles[1])
+                time.sleep(0.25)
+                self.door.stop()
                 # turn LED OFF
 
 
