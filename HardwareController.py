a282cb5ec95729f030c8a0a1755093f75ef2f197
Felix2268157 <2268157@bdeb.qc.ca>
2025-03-17 10:05:50 -0400

    servo led

--- HardwareController.py
+++ HardwareController.py
@@ -1,65 +1,66 @@
 import RPi.GPIO as GPIO
+from gpiozero import Device, Servo
+from gpiozero.pins.pigpio import PiGPIOFactory
 import Freenove_DHT as DHT
 from Status import Status
 import time
 
 class HardwareController:
 
-    MAXIMUM_SAFE_TEMP = 21
+    MAXIMUM_SAFE_TEMP = 25
 
     def __init__(self, test_mode=False):
         self.is_test_mode = test_mode
         GPIO.setmode(GPIO.BCM)
+        Device.pin_factory = PiGPIOFactory('127.0.0.1')
 
         self.current_temp = 0
 
-        self.red_pin = "placeholder"
-        self.green_pin = "placeholder"
-        self.buzzer = 17
-        self.servo = 27
-        self.dht = 4
+        self.led = 19
+        self.buzzer = 4
+        self.servo = 18
+        self.dht = 24
         
         self.tones = [440, 1440]
         self.angles = [3, 12]
 
         GPIO.setup(self.buzzer, GPIO.OUT)
-        GPIO.setup(self.servo, GPIO.OUT)
-        GPIO.setup(self.red_pin, GPIO.OUT)
-        GPIO.setup(self.green_pin, GPIO.OUT)
+        self.door = Servo(self.servo)
+        GPIO.setup(self.led, GPIO.OUT)
+        self.door.detach()
 
-        GPIO.output(self.green_pin, GPIO.LOW)
-        GPIO.output(self.red_pin, GPIO.LOW)
+        GPIO.output(self.led, GPIO.LOW)
 
         self.alarm = GPIO.PWM(self.buzzer, 440)
-        self.door = GPIO.PWM(self.servo, 50)
         
-        self.door.start(0)
+        self.open_door()
         
         self.status = Status.NONE
         
     def read_temp(self):
-        self.temp_sensor = DHT.DHT(self.dht) 
+        self.temp_sensor = DHT.DHT(self.dht)
+        # On verifie si les donnees du capteur sont valides
         if self.temp_sensor.readDHT11() == 0:   
             self.current_temp = self.temp_sensor.getTemperature()
             
         return self.current_temp
 
     def check_temperature(self, ui_callback_function):
-        current_temp = self.read_temp()
-
         if not self.is_test_mode:
-            if current_temp > self.MAXIMUM_SAFE_TEMP and self.status != Status.ALERT:
-                print('Alert temp')
-                self.status = Status.ALERT
-                ui_callback_function("ALERT")
-                self.activate_alarm()
-                self.close_door()
+            self.current_temp = self.read_temp()
+            
+        if self.current_temp > self.MAXIMUM_SAFE_TEMP and self.status != Status.ALERT:
+            print('Alert temp')
+            self.status = Status.ALERT
+            ui_callback_function("ALERT")
+            self.activate_alarm()
+            self.close_door()
 
-            elif current_temp <= self.MAXIMUM_SAFE_TEMP and self.status != Status.SAFE:
-                print('Safe temp')
-                self.status = Status.SAFE
-                self.deactivate_alarm()
-                self.open_door()
+        elif self.current_temp <= self.MAXIMUM_SAFE_TEMP and self.status != Status.SAFE:
+            print('Safe temp')
+            self.status = Status.SAFE
+            self.deactivate_alarm()
+            self.open_door()
 
 
     def activate_test_mode(self):
@@ -70,29 +71,17 @@ class HardwareController:
 
     def activate_alarm(self):
         self.alarm.start(50)
-        self.activate_red_rgb()
+        GPIO.output(self.led, GPIO.HIGH)
 
     def deactivate_alarm(self):
         self.alarm.stop()
-        self.activate_green_rgb()
+        GPIO.output(self.led, GPIO.LOW)
 
     def close_door(self):
-        self.door.start(self.angles[0])
-        time.sleep(0.25)
-        self.door.stop()
+        self.door.max()
 
     def open_door(self):
-        self.door.start(self.angles[1])
-        time.sleep(0.25)
-        self.door.stop()
-
-    def activate_green_rgb(self):
-        GPIO.output(self.red_pin, GPIO.LOW)
-        GPIO.output(self.green_pin, GPIO.HIGH)
-
-    def activate_red_rgb(self):
-        GPIO.output(self.green_pin, GPIO.LOW)
-        GPIO.output(self.red_pin, GPIO.HIGH)
+        self.door.min()
 
     def increase_temp(self):
         self.current_temp += 1
--- SurveillanceSystem.py
+++ SurveillanceSystem.py
@@ -47,39 +47,39 @@ class SurveillanceSystem:
         self.deactivate_button.grid(row=4, column=1, pady=10)
 
         tkinter.Label(self.root, text="Température:", font=("Arial", 12),
-                      fg="white", bg="#2C2F33").pack()
+                      fg="white", bg="#2C2F33").grid(row=5, column=1, pady=10)
 
         self.increase_temp_btn = tkinter.Button(self.root, text="+", command=self.increase_temp)
-        self.increase_temp_btn.pack()
+        self.increase_temp_btn.grid(row=6, column=1, pady=10)
 
         self.decrease_temp_btn = tkinter.Button(self.root, text="-", command=self.decrease_temp)
-        self.decrease_temp_btn.pack()
+        self.decrease_temp_btn.grid(row=7, column=1, pady=10)
 
         tkinter.Label(self.root, text="Trappe:", font=("Arial", 12),
-                      fg="white", bg="#2C2F33").pack()
+                      fg="white", bg="#2C2F33").grid(row=8, column=1, pady=10)
 
         self.open_door_btn = tkinter.Button(self.root, text="Ouvrir", command=self.open_door)
-        self.open_door_btn.pack()
+        self.open_door_btn.grid(row=9, column=1, pady=10)
 
         self.close_door_btn = tkinter.Button(self.root, text="Fermer", command=self.close_door)
-        self.close_door_btn.pack()
+        self.close_door_btn.grid(row=10, column=1, pady=10)
 
         tkinter.Label(self.root, text="Alarme:", font=("Arial", 12),
-                      fg="white", bg="#2C2F33").pack()
+                      fg="white", bg="#2C2F33").grid(row=11, column=1, pady=10)
 
         self.activate_alarm_btn = tkinter.Button(self.root, text="Activer", command=self.activate_alarm)
-        self.activate_alarm_btn.pack()
+        self.activate_alarm_btn.grid(row=12, column=1, pady=10)
 
         self.deactivate_alarm_btn = tkinter.Button(self.root, text="Désactiver", command=self.deactivate_alarm)
-        self.deactivate_alarm_btn.pack()
+        self.deactivate_alarm_btn.grid(row=13, column=1, pady=10)
 
     def update_temperature(self):
-        temp = self.hardware.read_temp()
+        temp = self.hardware.current_temp
         self.temperature_value.config(text=f"{temp}°C")
 
         self.hardware.check_temperature(self.handle_alert)
 
-        self.root.after(2000, self.update_temperature)
+        self.root.after(500, self.update_temperature)
 
     def handle_alert(self, status):
         if status == "ALERT":
